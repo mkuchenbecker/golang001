6dd3153af5bbdfa42c8c43a7e63f1bc212aa996c
mkuchenbecker <mkuchenbecker@lyft.com>
2018-10-12 08:46:58 -0700

    .

--- deltav/shuttle/main/main.go
+++ deltav/shuttle/main/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"log"
-	"time"
 
 	protos "github.com/golang001/deltav/protos"
+	"github.com/golang001/deltav/shuttle"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
@@ -24,8 +23,17 @@ func main() {
 	defer conn.Close()
 	c := protos.NewWorldModelClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	vessel := protos.Vessel{
+		Sensors: &protos.SensorSystem{
+			Sensors: []*protos.Sensor{
+				&protos.Sensor{
+					SensorType:    protos.Sensor_PASSIVE,
+					RadiationType: protos.RadiationType_GAMMA,
+				},
+			},
+		},
+	}
+	playerShuttle := shuttle.NewPlayerShuttle(vessel, c)
 	r, err := c.Register(ctx, &protos.RegisterRequest{})
 	if err != nil {
 		log.Fatalf("could not register: %v", err)
