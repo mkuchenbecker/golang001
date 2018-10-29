1afe143c613c4be83e249eb9df14efa6009cc6af
Mike Kuchenbecker <mkuchenbecker@lyft.com>
2018-10-29 16:53:15 -0700

    working out models of driver

--- deltav/protos/position.go
+++ deltav/protos/position.go
@@ -2,10 +2,14 @@ package deltav_protos
 
 import "math"
 
-func (p *Position) MagnitudeSquared() float64 {
+func (p *Vector3) MagnitudeSquared() float64 {
 	return math.Pow(p.X, 2) + math.Pow(p.Y, 2) + math.Pow(p.Z, 2)
 }
 
+func (p *Vector3) Subtract(n *Vector3) *Vector3 {
+	return &Position{X: p.X - n.X, Y: p.Y - n.Y, Z: p.Z - n.ZT}
+}
+
 func (p *Position) Subtract(n *Position) *Position {
-	return &Position{X: p.X - n.X, Y: p.Y - n.Y, Z: p.Z - n.Z, T: p.T - n.T}
+	return &Position{Position: p.Position.Subtract(n.position), T: p.T - n.T}
 }
